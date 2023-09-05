76a7c98c9957a5e7ccc711d4e7b1359f97adb2f3
olya1071111 <104586687+olya1071111@users.noreply.github.com>
2023-09-05 20:02:05 +0300

    Add files via upload

---
+++ src/com/itacademy/less17/main/Animal.java
@@ -0,0 +1,8 @@
+package com.itacademy.less17.main;
+
+public class Animal {
+
+	public void sound() {
+		System.out.println("ку-ку");
+	}
+}
---
+++ src/com/itacademy/less17/main/Cat.java
@@ -0,0 +1,8 @@
+package com.itacademy.less17.main;
+
+public class Cat extends Animal{
+	public void sound() {
+		System.out.println("мяу-мяу");
+	}
+
+}
